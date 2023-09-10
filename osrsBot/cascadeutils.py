68b6c5195331dc1d43dd50410a5d4d37dca2032c
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-09-10 15:25:22 -0400

    removed files

--- osrsBot/cascadeutils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Generate Negative Description File
-def gndf():
-    with open('neg.txt', "w") as f:
-        for filename in os.listdir("negative"):
-            f.write("negative/" + filename + "\n")
